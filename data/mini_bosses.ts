e4afa300e47f4dde9422bd0f00e8c3d318f7981e
Ishan Rayeen <ishan0445@gmail.com>
2024-01-07 12:52:39 +0200

    Added bosses data

---
+++ data/mini_bosses.ts
@@ -0,0 +1,77 @@
+import { ListType } from "./types";
+const prefix = process.env.NEXT_PUBLIC_BASE_PATH || "";
+
+export const MiniBossesList: Array<ListType> = [
+  {
+    "id": "06d31a01-979c-4af5-83c5-3975beaba85f",
+    "imageUrl": prefix + "/mini_bosses/mad_donkey.jpg",
+    "name": "Mad Donkey",
+    "description": "Location: Alchemist Bridge"
+  },
+  {
+    "id": "9054ea62-db93-4fd0-ade8-cb845fea8a12",
+    "imageUrl": prefix + "/mini_bosses/puppet_of_the_future.jpg",
+    "name": "Puppet of the Future",
+    "description": "Location: Workshop Union Culvert"
+  },
+  {
+    "id": "4235a4fa-502f-463c-b612-f59d0a7bad24",
+    "imageUrl": prefix + "/mini_bosses/survivor.jpg",
+    "name": "Survivor",
+    "description": "Location: Venigni Works Control Room"
+  },
+  {
+    "id": "f8e17d45-1896-406d-bc34-c556496a831f",
+    "imageUrl": prefix + "/mini_bosses/the_atoned.jpg",
+    "name": "The Atoned",
+    "description": "Location: Moonlight Town (Pilgrim's Cable Railway)"
+  },
+  {
+    "id": "739188f0-2800-4cfb-ad1a-395d5b8ad08b",
+    "imageUrl": prefix + "/mini_bosses/white_lady.jpg",
+    "name": "White Lady",
+    "description": "Location: Rosa Isabelle Street"
+  },
+  {
+    "id": "1c0fbe58-e5e3-4821-b129-b3e1d0011728",
+    "imageUrl": prefix + "/mini_bosses/mad_clown_puppet.jpg",
+    "name": "Mad Clown Puppet",
+    "description": "Location: Rosa Isabelle Street"
+  },
+  {
+    "id": "25f034db-f7d5-4508-b0df-a24bb8a5259f",
+    "imageUrl": prefix + "/mini_bosses/owl_doctor.jpg",
+    "name": "Owl Doctor",
+    "description": "Location: Barren Swamp"
+  },
+  {
+    "id": "85426d96-75f8-4c33-b33b-0a7c916b0887",
+    "imageUrl": prefix + "/mini_bosses/walker_of_illusions.jpg",
+    "name": "Walker of Illusions",
+    "description": "Location: Collapsed Workshop Tower"
+  },
+  {
+    "id": "439cb7da-349a-4147-843c-1a9fade6768e",
+    "imageUrl": prefix + "/mini_bosses/door_guardian.jpg",
+    "name": "Door Guardian",
+    "description": "Location: Arche Abbey Entrance"
+  },
+  {
+    "id": "fad933ec-fb01-409f-bc16-e24d07e1f951",
+    "imageUrl": prefix + "/mini_bosses/robber_weasel.jpg",
+    "name": "Robber Weasel",
+    "description": "Location: Krat Central Station Platform"
+  },
+  {
+    "id": "e6b52fef-4fc2-4999-865c-7835a6af0f7f",
+    "imageUrl": prefix + "/mini_bosses/red_fox.png",
+    "name": "Red Fox",
+    "description": "Location: Arche Abbey Upper Area"
+  },
+  {
+    "id": "aad5599f-5d14-45ce-99dc-4c3d999a5abf",
+    "imageUrl": prefix + "/mini_bosses/black_cat.png",
+    "name": "Black Cat",
+    "description": "Location: Arche Abbey Outer Wall"
+  }
+];
