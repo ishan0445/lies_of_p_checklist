9f06ff753175aab82460218365332ed424b1f7b9
ishan0445 <ishan0445@gmail.com>
2024-01-07 11:53:48 +0000

    Deploying to gh-pages from @ ishan0445/lies_of_p_checklist@0cfa3ee3b76d8f2c877db25dbec55a6d8961102a 🚀

---
+++ _next/static/qVFwGziKxctoWu6KWO8sN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/818-8f19fce1e600b629.js","static/chunks/pages/index-6cc149dcde00b816.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
